b2045c34efaa8ebe0af31857d51014fb81754205
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-09-23 20:07:50 +0530

    import java.util.Scanner; class mull {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         float a=s.nextFloat();         float b=s.nextFloat();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class mull
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        float a=s.nextFloat();
+        float b=s.nextFloat();
+        System.out.format("%.2f",a*b);
+    }
+}
