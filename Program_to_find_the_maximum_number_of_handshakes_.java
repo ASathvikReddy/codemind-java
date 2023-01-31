c6425ddf93ce37363bd9663982db35c30373beb3
ASREDDY <97426173+ASathvikReddy@users.noreply.github.com>
2023-01-31 12:00:00 +0530

    import java.util.*; class solution{     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         System.out.println((n*(n-1)/2));     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class solution{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.println((n*(n-1)/2));
+    }
+}
