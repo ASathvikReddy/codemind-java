e951bbf77f59596ee501893138dcc52b791b64aa
ASREDDY <97426173+ASathvikReddy@users.noreply.github.com>
2023-02-04 22:11:53 +0530

    import java.util.*; class A{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=0 ;         for(int i=1;i<=n;i++)         {             s+=i;         }         System.out.println(s);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class A{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=0 ;
+        for(int i=1;i<=n;i++)
+        {
+            s+=i;
+        }
+        System.out.println(s);
+    }
+}
